a1117939de8906abb58e8d8772921c9d156b4e3e
jaystino <jayottenstein@gmail.com>
2020-10-21 21:18:01 -0400

    Handle DB connection on startup/shutdown

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 
 from app.api import person
+from app.db import engine, metadata, database
 
+metadata.create_all(engine)
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 app.include_router(person.router)
