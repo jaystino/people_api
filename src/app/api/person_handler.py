4f0583f05d3fa98c7f77e39910657579891eb8e3
jaystino <jayottenstein@gmail.com>
2020-10-23 20:38:42 -0400

    Move person response formatting to fxn

--- src/app/api/person_handler.py
+++ src/app/api/person_handler.py
@@ -43,27 +43,84 @@ async def create_person(person_in: PersonCreateIn):
     version = 1
     is_latest = True
     try:
-        record_id = await insert_person(person_in, person_id, version, is_latest)
-        return dict(
-            record=record_id,
-            person_id=person_id,
-            first_name=person_in.first_name,
-            middle_name=person_in.middle_name,
-            last_name=person_in.last_name,
-            email=person_in.email,
-            age=person_in.age,
-            version=version,
-            is_latest=is_latest,
-        )
+        record = await insert_person(person_in, person_id, version, is_latest)
+        return _format_person_response(person_in, record, person_id, version, is_latest)
     except (DataError, NameError) as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.put("/", response_model=Person)
 async def update_person(person: PersonUpdateIn):
-    return Person
+    # fetch latest record
+    current_person = await select_person(person.person_id)
+    if not current_person:
+        message = (
+            "cannot update person, no records with person_id "
+            f"{person.person_id} found"
+        )
+        raise HTTPException(status_code=404, detail=message)
+    # ensure request contains updates compared to current_person
+    updates = {k: v for k, v in dict(person).items() if v and v != current_person[k]}
+    if not updates:
+        raise HTTPException(status_code=400, detail="no new attributes to update")
+
+    # set current version is_latest to False
+    # if not await update_latest(False, record=current_person.get("record")):
+    #     raise HTTPException(
+    #         status_code=500, detail="failed to deactivate previous version"
+    #     )
+    if not await update_latest(person_id=person.person_id):
+        raise HTTPException(
+            status_code=500, detail="failed to deactivate previous version"
+        )
+
+    # "update" new version
+    new_version = current_person.get("version") + 1
+    updated_person = PersonCreateIn(
+        first_name=updates.get("first_name", current_person.get("first_name")),
+        middle_name=updates.get("middle_name", current_person.get("middle_name")),
+        last_name=updates.get("last_name", current_person.get("last_name")),
+        email=updates.get("email", current_person.get("email")),
+        age=updates.get("age", current_person.get("age")),
+    )
+    record = await insert_person(
+        updated_person, person.person_id, new_version, is_latest=True
+    )
+
+    return _format_person_response(
+        updated_person, record, person.person_id, new_version, True
+    )
 
 
 @router.delete("/{person_id}", response_model=PersonDeleteOut)
 async def delete_person(person_id: UUID):
     return PersonDeleteOut
+
+
+def _format_person_response(
+    person_in: PersonCreateIn,
+    record: int,
+    person_id: UUID,
+    version: int,
+    is_latest: bool,
+) -> Dict:
+    """
+
+    :param person_in:
+    :param record:
+    :param person_id:
+    :param version:
+    :param is_latest:
+    :return:
+    """
+    return dict(
+        record=record,
+        person_id=person_id,
+        first_name=person_in.first_name,
+        middle_name=person_in.middle_name,
+        last_name=person_in.last_name,
+        email=person_in.email,
+        age=person_in.age,
+        version=version,
+        is_latest=is_latest,
+    )
