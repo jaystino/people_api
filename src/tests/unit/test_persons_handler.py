a54ab2f8ec8b5676bfb7c11233570716dd154f81
jaystino <jayottenstein@gmail.com>
2020-10-24 12:17:40 -0400

    Abstract async test helpers into separate module

--- src/tests/unit/test_persons_handler.py
+++ src/tests/unit/test_persons_handler.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from unittest import mock
 from uuid import UUID
 
@@ -8,39 +6,7 @@ import pytest
 from fastapi.exceptions import HTTPException
 
 from app.api import dal, persons_handler
-
-
-def async_mock(*args, **kwargs):
-    m = mock.MagicMock(*args, **kwargs)
-
-    async def mock_coro(*args, **kwargs):
-        return m(*args, **kwargs)
-
-    mock_coro.mock = m
-    return mock_coro
-
-
-def async_test(coro):
-    def wrapper(*args, **kwargs):
-        loop = asyncio.new_event_loop()
-        return loop.run_until_complete(coro(*args, **kwargs))
-
-    return wrapper
-
-
-class MockResponse:
-    def __init__(self, text, status):
-        self._text = text
-        self.status = status
-
-    async def text(self):
-        return self._text
-
-    async def __aexit__(self, exc_type, exc, tb):
-        pass
-
-    async def __aenter__(self):
-        return self
+from tests.unit.async_test_helpers import async_mock, async_test
 
 
 @async_test
